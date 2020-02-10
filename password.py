da790c3556fa03957be75071951a6c46b33cc7f0
italosyder <55523868+italosyder@users.noreply.github.com>
2020-02-09 21:14:28 -0300

    Create password.py

---
+++ password.py
@@ -0,0 +1,8 @@
+password= "mario874"
+insert= None
+while insert!=password:
+    insert = input("Insert the password  ")
+    if insert==password:
+        print("RIGHT")
+    else:
+        print("WRONG!!")
